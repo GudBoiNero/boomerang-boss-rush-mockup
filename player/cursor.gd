fd5a4a8c0bfeeee7cd83a2282cc0a6a7e062eec1
Benjamin Champagne <donkeykong797@gmail.com>
2025-04-21 21:39:08 -0400

    Update cursor.gd

--- player/cursor.gd
+++ player/cursor.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+@onready var player : PlayerController = get_parent()
+
 func _physics_process(delta: float) -> void:
 	var aim := PlayerInputService.get_aim()
 	position = aim * 100
-	look_at(aim + get_parent().global_position)
+	look_at(aim + player.global_position)
 	visible = aim != Vector2.ZERO
