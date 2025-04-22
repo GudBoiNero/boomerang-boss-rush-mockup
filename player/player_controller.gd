72ade2615b0b62f4d590634f67f184fba19d97cb
Benjamin Champagne <donkeykong797@gmail.com>
2025-04-21 21:31:49 -0400

    messing with boomerang variables

--- player/player_controller.gd
+++ player/player_controller.gd
@@ -4,6 +4,11 @@ class_name PlayerController
 @export_group("Velocity Layers")
 @export var momentum_velocity_layer : PlayerMomentumVelocityLayer
 
+@export_group("Boomerang")
+@export var br_distance_curve: CurveTexture
+@export var br_charge_time := 0.9
+@export var br_max_distance := 300.0
+
 var _velocity_layers : Array[VelocityLayer] = []
 
 func _ready() -> void:
