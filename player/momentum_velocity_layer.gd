048ec88bd013dbe53fdef9e8af146cf7f63cec31
Benjamin Champagne <donkeykong797@gmail.com>
2025-04-21 21:14:33 -0400

    mapped keys

--- player/momentum_velocity_layer.gd
+++ player/momentum_velocity_layer.gd
@@ -1,9 +1,14 @@
 extends VelocityLayer
 class_name PlayerMomentumVelocityLayer
 
-@export var SPEED := 4000.0
+@export var WALK_SPEED := 4000.0
+@export var RUN_SPEED := 12000.0
+@export var SPRINT_SPEED := 20000.0
+
+var _target_speed := 0.0
+var _speed := WALK_SPEED
 
 func _velocity_physics_process(delta: float) -> void:
 	var input := PlayerInputService.get_input_raw()
 	PlayerInputService.get_aim()
-	_velocity = input * delta * SPEED
+	_velocity = input * delta * _speed
