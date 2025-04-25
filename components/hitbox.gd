ed2e2ce4d1f11deedcb4e2ee63e80f80ef1682c2
Benjamin Champagne <donkeykong797@gmail.com>
2025-04-25 15:26:21 -0400

    hit/hurtbox testing

--- components/hitbox.gd
+++ components/hitbox.gd
@@ -1,3 +1,31 @@
+@tool
 @icon("res://textures/icon/hitbox.svg")
 extends Area2D
 class_name HitBox
+
+## list of HurtBoxes hit and not yet exited
+var _registered : Array[HurtBox]
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is HurtBox:
+		if area in _registered: return 
+		_registered.push_back(area)
+		print("enter")
+		area.hitbox_entered.emit(self)
+
+func _on_area_exited(area: Area2D) -> void:
+	if area is HurtBox:
+		if area not in _registered: return
+		_registered.remove_at(_registered.find(area))
+		print("exit")
+		area.hitbox_exited.emit(self)
+
+func _draw() -> void:
+	for child in get_children():
+		if child is CollisionShape2D:
+			child.debug_color = Color.RED
+			child.debug_color.a8 = 107
--- components/hurtbox.gd
+++ components/hurtbox.gd
@@ -1,3 +1,24 @@
+@tool
 @icon("res://textures/icon/hurtbox.svg")
 extends Area2D
 class_name HurtBox
+
+signal hitbox_entered(hitbox: HitBox)
+signal hitbox_exited(hitbox: HitBox)
+
+@export var invulnerable : bool = false
+
+func _ready() -> void:
+	hitbox_entered.connect(_on_hitbox_entered)
+	hitbox_exited.connect(_on_hitbox_exited)
+
+func _on_hitbox_entered(hitbox: HitBox) -> void:
+	pass
+
+func _on_hitbox_exited(hitbox: HitBox) -> void:
+	pass
+
+func _draw() -> void:
+	for child in get_children():
+		if child is CollisionShape2D or child is CollisionPolygon2D:
+			child.debug_color = Color.GRAY
