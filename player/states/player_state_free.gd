a3dd3333b8f744fa18dba64a8f0443e50533519f
Benjamin Champagne <donkeykong797@gmail.com>
2025-04-27 15:31:15 -0400

    there is no bug

--- player/states/player_state_free.gd
+++ player/states/player_state_free.gd
@@ -7,9 +7,6 @@ class_name PlayerStateFree
 func _ready() -> void:
 	state_entered.connect(enter)
 
-func _state_physics_process(delta: float) -> void:
-	print(v_momentum.get_velocity(), " : ", v_kb.get_velocity())
-
 func enter(from: State) -> void:
 	v_momentum.start()
 	v_kb.stop()
